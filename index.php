516af6ad4f97631b7899c9b26ec8261f489377fb
Maxsuel Fernando <maxsuel.dev@gmail.com>
2021-04-22 22:08:34 -0300

    update repo

---
+++ index.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<title>Maxsuel Fernando - Portfólio/Site 3</title>
+	<link rel="shortcut icon" href="../../img/faviconM.ico">
+	<meta charset="UTF-8">
+	<meta name="description" content="Maxsuel Fernando - Graduado em Sistemas de Informação, desenvolve páginas e sistemas Web.">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta property="og:url" content="https://maxsuelfernandob.000webhostapp.com/portfolio/site3"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:title" content="Maxsuel Fernando - Portfólio/Site 3">
+	<meta property="og:site_name" content="maxsuelfernandob">
+	<meta property="og:description" content="Maxsuel Fernando - Graduado em Sistemas de Informação, desenvolve páginas e sistemas Web."/>
+	<meta property="og:image" content="https://maxsuelfernandob.000webhostapp.com/portfolio/site3/img/about-mobile.png"/>
+	<meta property="og:image:type" content="image/png">
+	<meta property="og:image:width" content="800"> 
+	<meta property="og:image:height" content="600">
+
+	<!-- <link rel="stylesheet" href="../../css/normalize.css">
+	<link rel="stylesheet" href="../../css/reset.css">
+	<link rel="stylesheet" href="../../css/grid.css">
+	<link rel="stylesheet" href="css/style-raw.css">
+	<link rel="stylesheet" href="css/responsivo.css"> -->
+
+	<link rel="stylesheet" href="css/style.css?<?php echo time();?>">
+</head>
+<body>
+
+	<!-- Header -->
+
+	<header class="header">
+		<div class="container">
+			<div class="header-logo">
+				<a href="#" class="logo">logo</a>
+			</div>
+			<nav class="nav-menu">
+				<ul>
+					<li>
+						<a href="#intro" class="active">HOME</a>
+					</li>
+					<li>
+						<a href="#products">PRODUCTS</a>
+					</li>
+					<li>
+						<a href="#about">ABOUT US</a>
+					</li>
+					<li>
+						<a href="#contact">CONTACT</a>
+					</li>
+				</ul>
+				<img src="img/menu.png" class="menu" alt="Menu">
+			</nav>
+		</div>
+		<div class="nav-menu-responsivo">
+			<ul>
+				<li class="container">
+					<a href="#intro" class="active">HOME</a>
+				</li>
+				<li class="container">
+					<a href="#products">PRODUCTS</a>
+				</li>
+				<li class="container">
+					<a href="#about">ABOUT US</a>
+				</li>
+				<li class="container">
+					<a href="#contact">CONTACT</a>
+				</li>
+			</ul>
+		</div>
+	</header>
+
+	<!-- Intro -->
+	
+	<section id="intro" class="intro">
+		<div class="titulo-intro">
+			<h1>lorem</h1>
+		</div>
+		<div class="subtitulo-intro">
+			<h2>Lorem ipsum dolor sit amet</h2>
+		</div>
+		<div class="btn-intro">
+			<a href="#products">Products</a>
+		</div>		
+	</section>
+
+	<!-- Produtos -->
+
+	<section id="products" class="products">
+		<div class="container">
+			<div class="titulo-products">
+				<h3>PRODUCTS</h3>
+			</div>
+			<div class="conteudo-products">
+				<div class="grid-1-3 item-products">
+					<img src="img/products/item1.png" alt="Item 1">
+					<div class="descricao-item">
+						Lorem ipsum dolor sit amet
+					</div>
+					<div class="valor-item">
+						$129.00
+					</div>
+				</div>
+				<div class="grid-1-3 item-products">
+					<img src="img/products/item2.png" alt="Item 2">
+					<div class="descricao-item">
+						Lorem ipsum dolor sit amet
+					</div>
+					<div class="valor-item">
+						$189.00
+					</div>
+				</div>
+				<div class="grid-1-3 item-products">
+					<img src="img/products/item3.png" alt="Item 3">
+					<div class="descricao-item">
+						Lorem ipsum dolor sit amet
+					</div>
+					<div class="valor-item">
+						$129.00
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- About -->
+	
+	<section id="about" class="about">
+		<div class="container">
+			<h3 class="titulo-about">About us</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis laborum reiciendis aperiam incidunt esse perferendis similique cum accusamus voluptas vitae.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae quam temporibus, beatae delectus nesciunt sint ea qui. Voluptatibus atque quos, numquam porro esse illo quidem et iusto vel amet repellendus vero possimus sint ullam eligendi officiis molestias laudantium ad adipisci! Fugiat ea voluptates dolores minima laboriosam consequatur labore, nulla commodi architecto eius excepturi natus vitae praesentium dicta vero obcaecati magni.</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga non quo quasi aut, qui molestiae, excepturi at ipsa velit, eos incidunt animi sapiente error tenetur cum harum obcaecati veniam iste quibusdam?</p>
+		</div>
+	</section>
+
+	<!-- Contact -->
+
+	<section id="contact" class="contact">
+		<div class="container">
+			<ul>
+				<li class="grid-8 contact-col endereco">
+					<h3>CONTACT</h3>
+					<ul>
+						<li>
+							<p>+55 11 1234-5678</p>
+						</li>
+						<li>
+							<p>lorem@ipsum.com</p>
+						</li>
+						<li>
+							<p>Lorem Ipsum Dolor - Amet</p>
+						</li>
+					</ul>
+				</li>
+				<li class="grid-8 contact-col social">
+					<h3>SOCIAL NETWORKS</h3>
+					<ul>
+						<li>
+							<a href="#" target="_blank">
+								<img src="img/redes-sociais/facebook.png" alt="Facebook-logo">
+								</a>
+						</li>
+						<li>
+							<a href="#" target="_blank">
+								<img src="img/redes-sociais/instagram.png" alt="Instagram-logo">
+							</a>
+						</li>
+						<li>
+							<a href="#" target="_blank">
+								<img src="img/redes-sociais/twitter.png" alt="Twitter-logo">
+							</a>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</section>
+
+	<!-- Map -->
+
+	<section class="map">
+		
+	</section>
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<p>Lorem ipsum dolor sit amet</p>
+	</footer>
+	
+	<script src="../../js/libs/jquery-3.1.1.min.js"></script>
+	<script src="../../js/scrollPage.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$(".menu").on("click", function(){
+		$(".nav-menu-responsivo").toggleClass("showing")
+	});
+});
